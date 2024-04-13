4dcb41f5d92bf7f625de0024e784f6493bda84a8
Brunocribeiro01 <brunocribeiro01@gmail.com>
2024-04-12 22:16:28 -0300

    de novo

---
+++ 1prog.py
@@ -0,0 +1,12 @@
+nome = input ("Digite seu nome completo")
+#bruno cesar Ribeiro 
+
+nome = nome. upper ()
+#BRUNO CESAR RIBEIRO
+
+vetor_nomes = nome.split (" ")
+# [BRUNO, CESAR, RIBEIRO]
+
+print ("Primeiro nome: ", vetor_nomes [0])
+print ("Sobrenome: ", vetor_nomes [-1])
+
